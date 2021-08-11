f534dfa70f80e123b2561b49197b5581184f414b
Lokindra Dangi <pachar.com@gmail.com>
2021-08-11 22:47:21 +0545

    Footer section is complete

---
+++ components/Footer.js
@@ -0,0 +1,38 @@
+function Footer() {
+  return (
+    <div className="grid grid-cols md:grid-cols-4 gap-y-10 px-32 py-14 text-gray-600 bg-gray-100">
+      <div className="space-x-5 text-xs text-gray-800">
+        <h5 className="font-bold">ABOUT</h5>
+        <p>How Airbnb works</p>
+        <p>Newsroom</p>
+        <p>Airbnb Plus</p>
+        <p>Airbnb Luxe</p>
+      </div>
+      <div className="space-x-5 text-xs text-gray-800">
+        <h5 className="font-bold">COMMUNITY</h5>
+        <p>Accessibility</p>
+        <p>This is not a real site</p>
+        <p>It's a pretty awasome clone</p>
+        <p>Referal accepted</p>
+      </div>
+
+      <div className="space-x-5 text-xs text-gray-800">
+        <h5 className="font-bold">HOST</h5>
+        <p>Papa React</p>
+        <p>Presents</p>
+        <p>Reative Boyie</p>
+        <p>Hundred of Student</p>
+      </div>
+
+      <div className="space-x-5 text-xs text-gray-800">
+        <h5 className="font-bold">ABOUT</h5>
+        <p>How Airbnb works</p>
+        <p>Newsroom</p>
+        <p>Airbnb Plus</p>
+        <p>Airbnb Luxe</p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
